0aea638b61d1d703377453103263e5d3d508b14b
Cheri <avyishi@gmail.com>
2017-02-11 13:01:16 -0400

    Added record creation

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import mongodb from 'mongodb';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
+
 const MONGO_URI = 'mongodb://localhost/movies';
+
+function validate(data) {
+	let errors = {};
+	if (data.title === '') errors.title = "You cannot have an empty title";
+	if (data.photo === '') errors.photo = "You cannot have an empty Photo URL";
+	const isVal = Object.keys(errors).length === 0;
+	return { errors, isVal };
+
+}
 
 mongodb.MongoClient.connect(MONGO_URI, function(error, db) {
 
@@ -11,6 +23,24 @@ mongodb.MongoClient.connect(MONGO_URI, function(error, db) {
 		db.collection('movies').find({}).toArray((error, movies) => {
 			res.json({ movies });
 		});
+	});
+
+	app.post('/api/movies', (req, res) => {
+		const { errors, isVal } = validate(req.body);
+		if (isVal){
+			const { title, photo } = req.body;
+			db.collection('movies').insert({ title, photo }, (error, result) => {
+				if (error) {
+					res.status(500).json({ errors: { global: "Oops something is right!" }});
+				} else {
+					res.json({ movie: result.ops[0] });
+				} 
+			})
+
+		} else {
+			res.status(400).json({ errors });
+		}
+
 	});
   
 	app.use((req, res) => {
--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import { Redirect } from 'react-router';
+import { connect } from 'react-redux'
+import { saveMovie} from './actions';
 
 class MovieForm extends Component {
 	state = {
 		title: '',
 		photo: '',
-		errors: {}
+		errors: {},
+		loading: false,
+		complete: false
 	}
 
 	handleChange = (event) => {
@@ -26,31 +31,51 @@ class MovieForm extends Component {
 		if (this.state.title === '') errors.title = "You cannot have an empty title";
 		if (this.state.photo === '') errors.photo = "You cannot have an empty Photo URL";
 		this.setState({ errors });
+		const isVal = Object.keys(errors).length === 0
+
+		if (isVal) {
+			const { title, photo } = this.state;
+			this.setState({ loading: true });
+			this.props.saveMovie({ title, photo }).then(
+				() => { this.setState({ complete: true })},
+				(err) => err.response.json().then(({errors}) => this.setState({ errors, loading: false }))
+
+			);
+		}
 	}
 
 	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
+		const form = (
+			<form onSubmit={this.handleSubmit} className={classnames('form', { loading: this.state.loading })}>
 				<h1>Add new movie</h1>
-				<div className={classnames('formField', {error: !!this.state.errors.title})}>
+
+				{!!this.state.errors.global && <div className="not loading"><p>{this.state.errors.global}</p></div>}
+				
+				<div className={classnames('formField text-center', {error: !!this.state.errors.title})}>
 					<label htmlFor="title">Movie Title</label>
 					<input id="title" name="title" value={this.state.title} onChange={this.handleChange}/>
 					<span>{this.state.errors.title}</span>
 				</div>
-				<div className={classnames('formField', {error: !!this.state.errors.photo})}>
+				<div className={classnames('formField text-center', {error: !!this.state.errors.photo})}>
 					<label htmlFor="photo">Photo URL</label>
 					<input id="photo" name="photo" value={this.state.photo} onChange={this.handleChange}/>
 					<span>{this.state.errors.photo}</span>
 				</div>
-				<div className="formField">
+				<div className="formField text-center">
 					{this.state.photo !== '' && <img src={this.state.photo} alt="photo" />}
 				</div>
-				<div>
+				<div className="text-center">
 					<button className="btn btn-primary">Submit</button>
 				</div>
 			</form>
 		);
+		return (
+			<div>
+				{ this.state.complete ? <Redirect to="/movies"/> : form }
+				
+			</div>
+		);
 	}
 }
 
-export default MovieForm;
+export default connect(null, { saveMovie })(MovieForm);

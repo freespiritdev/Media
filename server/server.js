ba84d4da31945d118382ceb6fcc1a66a9213866d
Cheri <avyishi@gmail.com>
2017-02-14 20:32:03 -0400

    Updated routes for shows

--- server/server.js
+++ server/server.js
@@ -78,7 +78,66 @@ mongodb.MongoClient.connect(MONGO_URI, function(error, db) {
 			res.json({});
 		})
 	});
-  
+
+// TV SHows
+	app.get('/api/shows', (req, res) => {
+			db.collection('shows').find({}).toArray((error, shows) => {
+			res.json({ shows });
+		});	
+	});
+
+	app.post('/api/shows', (req, res) => {
+		const { errors, isVal } = validate(req.body);
+		if (isVal){
+			const { title, photo } = req.body;
+			db.collection('shows').insert({ title, photo }, (error, result) => {
+				if (error) {
+					res.status(500).json({ errors: { global: "Oops something is right!" }});
+				} else {
+					res.json({ show: result.ops[0] });
+				} 
+			})
+		} else {
+			res.status(400).json({ errors });
+		}
+	});
+
+	app.get('/api/shows/:_id', (req, res) => {
+		db.collection('shows').findOne( { _id: new mongodb.ObjectId(req.params._id) }, (err, shows) => {
+			res.json({ shows })
+		})
+	});
+
+	app.put('/api/shows/:_id', (req, res) => {
+		const { errors, isVal } = validate(req.body);
+
+		if (isVal) {
+			const { title, photo } = req.body;
+			db.collection('shows').findOneAndUpdate(
+				{ _id: new mongodb.ObjectId(req.params._id) },
+				{ $set: { title, photo }},
+				{ returnOriginal: false },
+				(err, result) => {
+					if (err) { res.status(500).json({ errors: { global: error }}); 
+
+					return; 
+				}
+					res.json({ show: result.value });
+				}
+			);
+		} else {
+			res.status(400).json({ errors });
+		}
+	});
+
+	app.delete('/api/shows/:_id', (req, res) => {
+		db.collection('shows').deleteOne({ _id: new mongodb.ObjectId(req.params._id) }, (error, r) => {
+			if (error) { res.status(500).json({ errors: { global: err }}); return; }
+
+			res.json({});
+		})
+	});
+
 	app.use((req, res) => {
 		res.status(404).json({
     		errors: {
